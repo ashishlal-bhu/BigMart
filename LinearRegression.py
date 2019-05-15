bfd9cdd2c491848273dab3914ef156c3676b231a
Ashish K Lal <ashishlal.cal@gmail.com>
2019-05-15 15:54:02 +0530

    Continued

--- LinearRegression.py
+++ LinearRegression.py
@@ -2,3 +2,6 @@ import pandas as pd
 
 train = pd.read_csv("C:\\Users\\STUDENT\\PycharmProjects\\BigMart\\Data\\train.csv")
 print(train)
+test = pd.read_csv("C:\\Users\\STUDENT\\PycharmProjects\\BigMart\\Data\\test.csv")
+print(test)
+
